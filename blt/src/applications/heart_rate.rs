cba4097d6b1c8ca7eee598f622a24c314b9f2c85
Francisco Javier Estrella Liébana <estrella@ujaen.es>
2022-02-16 20:22:23 +0100

    heart rate service server
    Signed-off-by: Francisco Javier Estrella Liébana <estrella@ujaen.es>

--- blt/src/applications/heart_rate.rs
+++ blt/src/applications/heart_rate.rs
@@ -4,12 +4,25 @@ use crate::{
 };
 use anyhow::Result;
 use async_trait::async_trait;
-use bluer::gatt::local::CharacteristicWrite;
+use bluer::gatt::local::{CharacteristicControlEvent, CharacteristicRead, CharacteristicWrite};
 use bluer::gatt::remote::Characteristic;
+use bluer::gatt::CharacteristicWriter;
 use bluer::Uuid;
+use futures::{pin_mut, FutureExt, StreamExt};
+use rand::Rng;
 use std::collections::HashMap;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::io::AsyncWriteExt;
+use tokio::sync::Mutex;
+use tokio::time::interval;
 
 include!("../../../resources/services/heart_rate.inc");
+
+const INITIAL_HEART_RATE_MEASURE: u16 = 80;
+const NOTIFICATION_INTERVAL: u64 = 7;
+const MAX_HEART_RATE: u16 = 250;
+const MIN_HEART_RATE: u16 = 60;
 
 pub struct HeartRate;
 
@@ -26,7 +39,18 @@ impl BltApplication for HeartRate {
             uuid::Uuid::try_from(SERVICE).unwrap(),
             SERVICE_NAME,
             vec![uuid::Uuid::try_from(HEART_RATE_MEASUREMENT_CHARACTERISTIC).unwrap()],
-            vec![ApplicationDescriptor::default_read()],
+            vec![Some(CharacteristicRead {
+                read: true,
+                fun: Box::new(|_| {
+                    let value = Arc::new(Mutex::new(vec![0x0, 0x0]));
+                    async move {
+                        let value = value.lock().await.clone();
+                        Ok(value)
+                    }
+                    .boxed()
+                }),
+                ..Default::default()
+            })],
             vec![Some(CharacteristicWrite {
                 write: false,
                 ..Default::default()
@@ -39,8 +63,47 @@ impl BltApplication for HeartRate {
         GattApplication::from(self.application_descriptor())
     }
 
-    async fn serve(&self, _application_handler: ApplicationHandler) -> Result<ApplicationHandler> {
-        todo!("write")
+    async fn serve(
+        &self,
+        mut application_handler: ApplicationHandler,
+    ) -> Result<ApplicationHandler> {
+        let mut receiver = blt_application::server_control_c_handler(&application_handler);
+
+        let mut characteristic_writer: Option<CharacteristicWriter> = None;
+        let characteristic_control = application_handler.pop_characteristic_control().unwrap();
+        let mut interval = interval(Duration::from_secs(NOTIFICATION_INTERVAL));
+
+        let mut heart_rate = INITIAL_HEART_RATE_MEASURE;
+        let mut value;
+
+        pin_mut!(characteristic_control);
+
+        'main_loop: loop {
+            tokio::select! {
+                _ = receiver.recv() => break 'main_loop,
+                evt = characteristic_control.next() => {
+                    match evt {
+                        Some(CharacteristicControlEvent::Notify(notifier)) => {
+                            characteristic_writer = Some(notifier);
+                        },
+                        _ => break,
+                    }
+                },
+                _ = interval.tick() => {
+                    heart_rate = generate_random_heart_rate_measure(&heart_rate);
+                    value = heart_rate_to_vector(&heart_rate);
+                    println!("Generated new random value: {:#3}.", heart_rate);
+                    if let Some(writer) = characteristic_writer.as_mut() {
+                        if let Err(err) = writer.write(&value).await {
+                            println!("Notification stream error: {}.", &err);
+                            characteristic_writer = None;
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(application_handler)
     }
 
     async fn exercise_characteristics(
@@ -75,6 +138,19 @@ impl BltApplication for HeartRate {
     }
 }
 
-fn vector_to_heart_rate(time: &[u8]) -> u16 {
-    ((time[0] as u16) << 8) + time[1] as u16
+fn vector_to_heart_rate(vector: &[u8]) -> u16 {
+    ((vector[0] as u16) << 8) + vector[1] as u16
+}
+
+fn heart_rate_to_vector(heart_rate: &u16) -> Vec<u8> {
+    vec![(heart_rate >> 8) as u8, *heart_rate as u8]
+}
+
+fn generate_random_heart_rate_measure(previous_value: &u16) -> u16 {
+    let mut rnd = rand::thread_rng();
+    let factor: f32 = *previous_value as f32 * rnd.gen_range(0.0..0.05);
+    let direction = rnd.gen_range(-1..2);
+    let change = (factor * direction as f32) as i16;
+    let value = (*previous_value as i16 + change) as u16;
+    value.min(MAX_HEART_RATE).max(MIN_HEART_RATE) as u16
 }
